508a24841035d53b05810b4e2179d0d5675ed08c
Aka-suvam <subhamrai100@gmail.com>
2024-02-22 12:00:41 +0530

    fix error

--- src/component/Section-4/Area.jsx
+++ src/component/Section-4/Area.jsx
@@ -1,16 +1,16 @@
-import Businesslaw from "../../../assets/BusinessLaw.svg";
+import BusinessLaw from "../../../assets/BusinessLaw.svg";
 import partnership from "../../../assets/partnershipLaw.svg";
 
 import realstate from "../../../assets/Realestate.svg";
-import Business from "../../../assets/Business2.svg";
+import Business2 from "../../../assets/Business2.svg";
 import landlord from "../../../assets/landlord.svg";
-import elder from "../../../assets/Elder.svg";
+import Elder from "../../../assets/Elder.svg";
 
 const Area=()=>{
     return(<div className="area">
         <h1>Area of Practices</h1>
         <div className="flex-img">
-        <img src={Businesslaw} alt="busi-law-pic"/>
+        <img src={BusinessLaw} alt="busi-law-pic"/>
         <p className="bus-law">Business Law</p>
         <img src={partnership} alt="partnership-pic"/>
 
@@ -19,11 +19,11 @@ const Area=()=>{
         <p className="estate">REAL ESTATE LAW</p>
         
 
-        <img src={Business} alt="Business-pic"/>
+        <img src={Business2} alt="Business-pic"/>
         <p className="buss">BUSINESS LAW</p>
         <img src={landlord} alt="landlord-pic"/>
         <p className="landlord">LANDLORD DISPUTES</p>
-        <img src={elder} alt="elder-pic"/>
+        <img src={Elder} alt="elder-pic"/>
         <p className="elder">ELDER ABUSE</p>
         </div>
 
